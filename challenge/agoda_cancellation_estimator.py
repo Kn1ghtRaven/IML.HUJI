d56d6943243f53d8fed4b3b4f9db9e771e0c0d46
Mor Nahum <73553899+Kn1ghtRaven@users.noreply.github.com>
2022-04-27 20:55:11 +0300

    updated challenge

--- challenge/agoda_cancellation_estimator.py
+++ challenge/agoda_cancellation_estimator.py
@@ -2,14 +2,16 @@ from __future__ import annotations
 from typing import NoReturn
 from IMLearn.base import BaseEstimator
 import numpy as np
+from xgboost import XGBClassifier
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVC
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import confusion_matrix, precision_score, f1_score
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
-
+from sklearn.cluster import KMeans
 
 
 class AgodaCancellationEstimator(BaseEstimator):
@@ -29,10 +31,16 @@ class AgodaCancellationEstimator(BaseEstimator):
         ----------
 
         """
-        # self._models = LogisticRegression(max_iter=1000)
-        # self._models = SVC(kernel='rbf', probability=False)
-        # self._model_names = "BRF SVM"
-        self._models = RandomForestClassifier()
+        self._models = [RandomForestClassifier(n_estimators=150)]#, XGBClassifier(booster='gbtree', learning_rate=0.1, max_depth=5, n_estimators=220), KNeighborsClassifier(n_neighbors=5, algorithm='ball_tree', leaf_size=100)]
+        # self._models = [XGBClassifier(booster='gbtree', learning_rate=0.1, max_depth=5, n_estimators=220)]
+        # self._models = [ KNeighborsClassifier(n_neighbors=5, algorithm='ball_tree', leaf_size=100)]
+        # xgb = XGBClassifier(booster='gbtree', learning_rate=0.1, max_depth=5,
+        #                     n_estimators=180)
+        # xgb.fit(x_train, y_train)
+        #
+        # y_pred_xgb = xgb.predict(x_test)
+        #
+        # acc_xgb = accuracy_score(y_test, y_pred_xgb)
         super().__init__()
 
     def _fit(self, X: np.ndarray, y: np.ndarray) -> NoReturn:
@@ -52,7 +60,9 @@ class AgodaCancellationEstimator(BaseEstimator):
 
         """
         # self._models.fit(X, y)
-        self._models.fit(X.astype('int'), y.astype('int'))
+
+        for model in self._models:
+            model.fit(X.astype('int'), y.astype('int'))
 
     def _predict(self, X: np.ndarray) -> np.ndarray:
         """
@@ -68,11 +78,26 @@ class AgodaCancellationEstimator(BaseEstimator):
         responses : ndarray of shape (n_samples, )
             Predicted responses of given samples
         """
-        return self._models.predict(X)
+        # return self._models.predict(X)
+        predicts = np.zeros((np.shape(X)[0], np.shape(self._models)[0]))
+        for index, model in enumerate(self._models):
+            predicts[:, index] = model.predict(X)
+        predicts = np.sum(predicts, axis=1)
+        number_of_votes = np.shape(self._models)[0]/2
+        predicts[predicts <= number_of_votes] = 0
+        predicts[predicts > number_of_votes] = 1
+        return predicts
 
     def present(self, X, y):
-        print(confusion_matrix(y, self.predict(X)))
-        return self._models.score(X, y)
+        pretictions = self.predict(X)
+        print(confusion_matrix(y, pretictions))
+        # return self._models.score(X, y)
+        acc = np.zeros((np.shape(self._models)[0]))
+        print("precision_score :" +str(precision_score(y, pretictions)))
+        print("f1_score :"+str(f1_score(y, pretictions)))
+        for index, model in enumerate(self._models):
+            acc[index] = model.score(X, y)
+        return np.mean(acc)
 
     def report(self, X_test, y_true):
         y_pred = self._models.predict(X_test)
--- challenge/agoda_cancellation_prediction.py
+++ challenge/agoda_cancellation_prediction.py
@@ -3,14 +3,18 @@ from challenge.agoda_cancellation_estimator import AgodaCancellationEstimator
 from IMLearn.utils import split_train_test
 import numpy as np
 import pandas as pd
+
 PENALTY_DELIMITER = "_"
 NUMBER_OF_DAYS_DELIMITER = "D"
 PENALTY_PER_NIGHT = "N"
 PENALTY_PERCENTILE = "P"
 import re
-import numpy
 
 def getPenalty(penaltyString):
+    def func(a,b,c):
+        return 0
+    if penaltyString == "UNKNOWN":
+        return func
     penaltyTypes = penaltyString.split("_")
     penaltyDictionary = dict()
     for clause in penaltyTypes:
@@ -39,8 +43,11 @@ def getPenalty(penaltyString):
 
     #The function that will calculate the cancelation penalty
     def calculatePenalty(numDays, price, bookingLength):
-        numpyDays = numpy.array(list(penaltyDictionary.keys())) - numDays
-        arg = numpy.amin(numpyDays[numpyDays > 0]) + numDays
+        numpyDays = np.array(list(penaltyDictionary.keys())) - numDays
+        if not any(numpyDays[numpyDays > 0]):
+            arg = np.amin(numpyDays) + numDays
+        else:
+            arg = np.amin(numpyDays[numpyDays > 0]) + numDays
         penaltyCategory, penaltyCost = penaltyDictionary[arg]
         penaltyCost = float(penaltyCost)
         if penaltyCategory == PENALTY_PER_NIGHT:
@@ -53,9 +60,15 @@ def getPenalty(penaltyString):
 
 
 def seriesPenalty(penaltyStringSerie, numDays, priceList, bookingLengthList):
+    priceList = priceList.to_numpy()
+    bookingLengthList = bookingLengthList.to_numpy()
     returnSerie = list()
     for lineIndex in range(penaltyStringSerie.size):
-        returnSerie.append(getPenalty(penaltyStringSerie[lineIndex])(numDays, priceList[lineIndex], bookingLengthList[lineIndex]))
+        func = getPenalty(penaltyStringSerie[lineIndex])
+        price = priceList[lineIndex]
+        bookingdays = bookingLengthList[lineIndex]
+        res = func(numDays, price, bookingdays)
+        returnSerie.append(res)
     return pd.Series(returnSerie)
 
 def load_test_set(filename: str):
@@ -70,6 +83,11 @@ def load_test_set(filename: str):
     for name in special_requests:
         test_data[name] = test_data[name].fillna(0)
     test_data['total_of_special_requests'] = sum([test_data[col] for col in special_requests])
+    # for the penaltys
+    test_data[['checkin_date', 'checkout_date']] = test_data[['checkin_date', 'checkout_date']].apply(pd.to_datetime)
+    test_data['days'] = (test_data["checkout_date"] - test_data["checkin_date"]).dt.days
+    test_data["penaltys"] = seriesPenalty(test_data['cancellation_policy_code'], 11, test_data['original_selling_amount'], test_data['days'])
+    test_data['days_in_advance'] = (test_data['checkin_date'] - test_data['booking_datetime']).dt.days
     return test_data
 
 
@@ -82,16 +100,17 @@ def load_data_test(filename: str):
                           'request_latecheckin',
                           'request_highfloor', 'request_largebed',
                           'request_twinbeds', 'request_airport',
-                          'request_earlycheckin', "cancellation_datetime",'checkin_date', 'checkout_date', 'booking_datetime']]
+                          'request_earlycheckin', "cancellation_datetime", 'checkin_date', 'checkout_date', 'booking_datetime'
+                        ,'cancellation_policy_code']]
     para_list = ["origin_country_code", "original_payment_method"]
     features = categorail_var(features, para_list)
-    # full_data[['checkin_date', 'checkout_date', "cancellation_datetime"]] = full_data[['checkin_date', 'checkout_date', "cancellation_datetime"]].apply(pd.to_datetime)
-    # full_data['days'] = (full_data["checkout_date"] - full_data["checkin_date"]).dt.days
-    # features["penalty"] = seriesPenalty(full_data['cancellation_policy_code'],11,full_data['original_selling_amount'],full_data['days'])
+    features[['checkin_date', 'checkout_date', "cancellation_datetime"]] = features[['checkin_date', 'checkout_date', "cancellation_datetime"]].apply(pd.to_datetime)
+    features['days'] = (features["checkout_date"] - features["checkin_date"]).dt.days
+    features["penaltys"] = seriesPenalty(features['cancellation_policy_code'], 11, features['original_selling_amount'], features['days'])
     features['cancellation_time'] = features['cancellation_datetime'].fillna(features['checkin_date'])  # need to see if this line works
     features[['checkin_date', 'checkout_date', 'booking_datetime', 'cancellation_time']] = features[['checkin_date', 'checkout_date', 'booking_datetime','cancellation_time']].apply(pd.to_datetime)
     features['time_to_cancel'] = (features['cancellation_time'] - features['booking_datetime']).dt.days  # need to see if it is possitive or negative
-    features = features[features['time_to_cancel'] < 12]# need to recheck this line
+    # features = features[features['time_to_cancel'] < 12]# need to recheck this line
     special_requests = ['request_nonesmoke', 'request_latecheckin',
                         'request_highfloor', 'request_largebed',
                         'request_twinbeds', 'request_airport',
@@ -106,86 +125,16 @@ def load_data_test(filename: str):
     features['total_of_special_requests'] = sum(
         [features[col] for col in special_requests])
     features["cancellation_datetime"] = features["cancellation_datetime"].fillna(0)
-    features["cancellation_datetime"][features["cancellation_datetime"] != 0] = 1
+    # features["cancellation_datetime"][features["cancellation_datetime"] != 0] = 1
+    features.loc[features['cancellation_datetime'] != 0, 'cancellation_datetime'] = 1
     features['days'] = (features['checkout_date'] - features['checkin_date']).dt.days
     features['days_in_advance'] = (features['checkin_date'] - features['booking_datetime']).dt.days
     labels = features["cancellation_datetime"]
     features = features.drop(
-            columns=["cancellation_datetime", 'checkin_date', 'checkout_date', 'booking_datetime'])
+            columns=["cancellation_datetime", 'checkin_date', 'checkout_date', 'booking_datetime', 'cancellation_policy_code'])
     features = features.drop(
         columns=no_null)
     return features, labels
-
-def load_data(filename: str):
-    full_data = pd.read_csv(filename).drop_duplicates()
-    features = full_data[["original_payment_method",
-                          "hotel_star_rating",
-                          "no_of_adults", "no_of_children", "no_of_extra_bed",
-                          "no_of_room", "origin_country_code", "original_selling_amount",
-                                                               'request_nonesmoke',
-                          'request_latecheckin',
-                          'request_highfloor', 'request_largebed',
-                          'request_twinbeds', 'request_airport',
-                          'request_earlycheckin'
-                          ]]
-    para_list = ["origin_country_code", "original_payment_method"]
-    features = categorail_var(features, para_list)
-    full_data[['checkin_date', 'checkout_date']] = full_data[['checkin_date', 'checkout_date']].apply(pd.to_datetime)
-    special_requests = ['request_nonesmoke', 'request_latecheckin',
-                        'request_highfloor', 'request_largebed',
-                        'request_twinbeds', 'request_airport',
-                        'request_earlycheckin']
-    features['total_of_special_requests'] = sum([features[col] for col in special_requests])
-    features['days'] = (full_data["checkout_date"] - full_data["checkin_date"]).dt.days
-    # features["penalty"] = seriesPenalty(full_data['cancellation_policy_code'],11,full_data['original_selling_amount'],full_data['days'])
-    return features
-
-# def load_data(filename: str):
-#     """
-#     Load Agoda booking cancellation dataset
-#     Parameters
-#     ----------
-#     filename: str
-#         Path to house prices dataset
-#
-#     Returns
-#     -------
-#     Design matrix and response vector in either of the following formats:
-#     1) Single dataframe with last column representing the response
-#     2) Tuple of pandas.DataFrame and Series
-#     3) Tuple of ndarray of shape (n_samples, n_features) and ndarray of shape (n_samples,)
-#     """
-#     full_data = pd.read_csv(filename).drop_duplicates()
-#     features = full_data[["booking_datetime", "checkin_date", "checkout_date",
-#                            "hotel_country_code", "hotel_star_rating",
-#                           "accommadation_type_name", "charge_option", "customer_nationality",
-#                           "guest_is_not_the_customer", "guest_nationality_country_name",
-#                           "no_of_adults", "no_of_children", "no_of_extra_bed",
-#                           "no_of_room", "origin_country_code", "original_selling_amount",
-#                            "is_user_logged_in",
-#                           # "cancellation_policy_code",
-#                           "is_first_booking",
-#                           "request_nonesmoke", "request_latecheckin",
-#                           "request_highfloor", "request_largebed", "request_twinbeds",
-#                           "request_airport", "request_earlycheckin"]]
-#
-#     features.dropna()
-#     features[['checkin_date', 'checkout_date', 'booking_datetime']] = features[['checkin_date', 'checkout_date', 'booking_datetime']].apply(pd.to_datetime)
-#     features['days'] = (features['checkout_date'] - features['checkin_date']).dt.days
-#     features['days_in_advance'] = (features['checkin_date'] - features['booking_datetime']).dt.days
-#     # features["penalty"] = getPenalty(features['cancellation_policy_code'])(11,features['original_selling_amount'],features['days'])
-#     # features['charge_option'].replace(['Pay Later', 'Pay Now'], [0, 1], inplace=True)
-#     # features['is_first_booking'].replace(['False', 'True'], [0, 1], inplace=True)
-#     # features['is_user_logged_in'].replace(['False', 'True'], [0, 1], inplace=True)
-#     # features = categorail_var(features, "customer_nationality")
-#     para_list = ["customer_nationality", "accommadation_type_name", "hotel_country_code", "guest_nationality_country_name", "charge_option", "is_first_booking", "is_user_logged_in", "origin_country_code"]
-#     features = features.drop(columns=["checkin_date", "booking_datetime", "checkout_date"])
-#     features = categorail_var(features, para_list)
-#     features = features.fillna(0)
-#     labels = full_data["cancellation_datetime"]
-#     labels = labels.fillna(0)
-#     labels[labels != 0] = 1
-#     return features, labels
 
 def categorail_var(features , str_var_list):
     for str_var in str_var_list:
@@ -220,7 +169,7 @@ if __name__ == '__main__':
     # Load data
     df, cancellation_labels = load_data_test("../datasets/agoda_cancellation_train.csv")
     train_X, train_y, test_X, test_y = split_train_test(df, cancellation_labels)
-    test = load_test_set("../challenge/testsetweekly/test_set_week_3.csv")
+    test = load_test_set("../challenge/testsetweekly/test_set_week_4.csv")
     # Fit model over data
     cols = train_X.columns.intersection(test.columns)
     estimator = AgodaCancellationEstimator().fit(train_X[cols], train_y)
